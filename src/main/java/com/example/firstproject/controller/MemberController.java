aa7e103440079850fb6136b46a995b3a4dad60da
Park Seeun <park.seeun@gmail.com>
2025-04-07 20:09:54 +0900

    회원가입페이지

---
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -0,0 +1,70 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.MemberForm;
+import com.example.firstproject.entity.Member;
+import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+
+@Slf4j
+@Controller
+public class MemberController {
+    @Autowired
+    private MemberRepository memberRepository;
+    @GetMapping("/signup")
+    public String newMemberForm(){
+        return "members/new";
+    }
+
+    @PostMapping("/join")
+    public String createMember(MemberForm form){
+        System.out.println(form.toString());
+        //1.dto를 엔티티로 변환
+        Member member= form.toEntity();
+        System.out.println(member.toString());
+        //2.리파지터리로 엔티티를 db에 저장
+        Member saved=memberRepository.save(member);
+        System.out.println(saved.toString());
+        return "redirect:/articles/"+saved.getId();
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id = "+id);
+        Member memberEntity=memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", memberEntity);
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model){
+        ArrayList<Member> memberEntityList = memberRepository.findAll();
+        model.addAttribute("memberList", memberEntityList);
+        return "members/index";
+    }
+
+    @GetMapping("/members/{id}/edit")
+    public String edit(@PathVariable Long id, Model model){
+        Member memberEntity=memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", memberEntity);
+        return "members/edit";
+    }
+
+    @PostMapping("/members/update")
+    public String update(MemberForm form){
+        log.info(form.toString());
+        Member memberEntity=form.toEntity();
+        Member target=memberRepository.findById(memberEntity.getId()).orElse(null);
+        if(target!=null){
+            memberRepository.save(memberEntity);
+        }
+        return "redirect:/members/"+memberEntity.getId();
+    }
+}
---
+++ src/main/java/com/example/firstproject/dto/MemberForm.java
@@ -0,0 +1,19 @@
+package com.example.firstproject.dto;
+
+import com.example.firstproject.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class MemberForm {
+    private Long id;
+    private String email;
+    private String password;
+
+    public Member toEntity() {
+        return new Member(null, email, password);
+    }
+}
